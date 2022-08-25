950ef65f66cf2350f8ea8328ef34a1990049ec19
bq21 <bq21@ic.ac.uk>
2022-08-25 12:45:46 +0100

    Implemented working object-oriented implementation of secure aggregation for use case

--- safe-experiments/MoodAppUser.py
+++ safe-experiments/MoodAppUser.py
@@ -1,16 +1,41 @@
-from typing import List
+from typing import List, Dict
+from nptyping import NDArray, Float
+from itertools import repeat
 from collections import Counter
+import random as r
+import numpy as np
 
 class MoodAppUser:
     POSSIBLE_KEYWORDS = [0, 1, 2, 3, 4, 5, 6]
+    D_Value = None # replace with some default value.
 
     def __init__(self, id:int, inputs: List[int]) -> None:
-        self.id = id
-        self.document_set = inputs
-        self.no_of_responses = len(inputs)
+        # initialized on instance construction
+        self.id: int = id
+        self.other_user_ids: List[int] = []
+        self.document_set: List[int] = inputs
+        self.no_of_responses: int = len(inputs)
         self.keyword_frequencies = Counter(inputs)
-        self.feature_vector = []
-        self.generate_feature_vector() 
+        self.feature_vector: NDArray[Float] = [] # List converted to NDArray
+        self.generate_feature_vector()
+        # initialized when protocol starts
+        self.shares_to_send: Dict[int, NDArray[Float]] = {}
+        self.Nth_share: NDArray[Float] = None
+        self.shares_received: List[NDArray[Float]] = []
+        self.obfuscated_feature_v: NDArray[Float] = None 
+    
+    @classmethod
+    def set_D_value(cls, value_is_random = False, 
+                   shared_seed = None, PRNG_seeded = False) -> None:
+        """
+        * sets the class attribute 'D_Value' for all instances (users) to use for a round of the safe Protocol
+
+        * params with default args do only need to be changed if we want to start randomizing the D value each round.
+        * 'value_is_random' flag determines whether the D value is to be generated in a randomized way. If so, the 'shared_seed' should be passed in. Once the PRNG has been seeded, then a new PRNG no longer needs to be selected and can just be generated afresh.
+        """
+        if not value_is_random:
+            cls.D_Value = 100.0
+            return
 
     def generate_missing_keyword_frequencies(self) -> None:
         """
@@ -33,6 +58,52 @@ class MoodAppUser:
         """
         self.generate_missing_keyword_frequencies()
         for frequency in self.keyword_frequencies.values():
-            self.feature_vector.append(round(frequency / self.no_of_responses, 4))    
+            self.feature_vector.append(frequency / self.no_of_responses)
+        self.feature_vector = np.round(self.feature_vector, 4)
 
-# TODO : will want to define the 'generate_shares' method for each user. And essentially make the run object oriented.
+    def generate_shares_to_send(self, number_of_users: int,
+                                  share_dimensionality: int) -> None:
+        """
+        * assigns a dictionary {id:share} to the instance attribute 'shares_to_send'
+
+        * N-1 shares are uniformly drawn from an interval [-D_value, D_value]
+        * Each share is a numpy array that is of size == share_dimensionality. For simplicity, a one-dimensional numpy array with k columns represents the k-dimensional share vector. Numpy arrays are more efficient than Python lists in this case.
+        * These N-1 shares are for the other N-1 users.
+        """
+        self.other_user_ids = [id for id in range(number_of_users) if id != self.id]
+        for other_user in self.other_user_ids:
+            share_to_distribute = [round(r.uniform(-self.D_Value, self.D_Value), 4) 
+                                      for _ in repeat(None, share_dimensionality)]
+            share_to_distribute = np.array(share_to_distribute)
+            self.shares_to_send[other_user] = share_to_distribute
+
+    def calculate_Nth_share(self) -> None:
+        """
+        * assigns to the instance attribute 'Nth_share'
+
+        * calculates the Nth_share by subtracting the sum of the N-1 shares drawn from the [-D, D] interval from their raw_feature vector
+        """
+        sum_of_shares_to_send = np.sum(list(self.shares_to_send.values()), axis = 0)
+        self.Nth_share = np.subtract(self.feature_vector, sum_of_shares_to_send)  
+
+    def get_share_for_user(self, user_id: int): # NOTE -> NDArray[Float] throws error for some reason 
+        """
+        * indexes the 'shares_to_send' instance attribute with the given user_id and returns the share_to_be_distributed
+        """
+        return self.shares_to_send[user_id]
+
+    def receive_share(self, share) -> None: 
+        """
+        * appends to the 'share_received' instance attribute
+        """
+        self.shares_received.append(share)
+
+    def generate_obfuscated_feature_v(self) -> None:
+        """
+        * assigns to the 'obfuscated_feature_v' instance attribute
+        
+        * uses the shares_received and the Nth share to do this.
+        """
+        sum_of_received_shares = np.sum(self.shares_received, axis = 0)
+        self.obfuscated_feature_v = np.add(self.Nth_share, sum_of_received_shares)
+        self.obfuscated_feature_v = np.round(self.obfuscated_feature_v, 4)
